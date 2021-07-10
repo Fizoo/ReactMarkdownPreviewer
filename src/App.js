0b10ccb4b9fad2c04328e1e4b48b2307718ee0d0
Oleg <oleg.mykytchuk@gmail.com>
2021-07-10 20:17:28 +0300

    q

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Editor from "./component/Editor";
+import Preview from "./component/Preview";
+import {placeholder} from "./component/placeholder";
+
+
+let marked = require('marked')
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [text, setText] = useState(placeholder)
+    const [showA, setShowA] = useState(true);
+    const [classNames, setClassNames] = useState('')
+
+    const expander = () => {
+        setShowA(!showA);
+        showA ? setClassNames('men') : setClassNames('')
+    }
+    const handleChange = (e) => {
+        setText(e.target.value);
+    }
+    return (
+        <div className='main'>
+            <Editor expander={expander}
+                    handleChange={handleChange}
+                    text={text}
+                    classNames={classNames}
+            />
+            {showA &&
+            <Preview
+                marked={marked}
+                text={text}/>
+            }
+
+        </div>
+    )
 }
+
 
 export default App;
